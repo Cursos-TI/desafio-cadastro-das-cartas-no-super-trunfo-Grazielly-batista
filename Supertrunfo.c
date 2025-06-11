c4eb9d483ecd62d1c8414e1626adadebec08a991
Grazielly-batista <batistasantosgrazielly@gmail.com>
2025-06-11 14:13:34 +0000

    Super_trunfo.c

--- Supertrunfo.c
+++ Supertrunfo.c
@@ -41,13 +41,41 @@ printf("Digite a populacao:");
 scanf("%lu", &populacao_carta2);
 printf("Digite a area (em Km2):");
 scanf("%f", &area_carta2);
-printf("Digite o pib (em trilhoes)");
+printf("Digite o pib (em trilhoes):");
 scanf("%f", &pib_carta2);
 printf("Digite o numero de pontos turisticos:");
 scanf("%d", &pontos_turisticos_carta2);
 densidade_populacional_carta2 = (float)populacao_carta2 / area_carta2;
 pib_per_capita_carta2 = pib_carta2 / populacao_carta2;
 super_poder_carta2 = (float)populacao_carta2 + area_carta2 + pib_carta2 + pontos_turisticos_carta2 + (1.0f / densidade_populacional_carta2) + pib_per_capita_carta2;
+printf("\n--- dados da carta 1 (%s) ---\n" , codigo_carta1);
+printf("populacao: %lu\n", populacao_carta1);
+printf("area: %.2f km2\n", area_carta1);
+printf("pib: %.2f trilhoes: \n", pib_carta1);
+printf("pontos turisticos: %d\n", pontos_turisticos_carta1);
+printf("densidade populacional: %.2f hab/km2\n", densidade_populacional_carta1);
+printf("pib per capita: %.2f trilhoes/hab\n", pib_per_capita_carta1);
+printf("super poder: %.2f\n", super_poder_carta1);
+//exibiçao carta 2
+printf("\n--- dados da carta 2 (%s) ---\n", codigo_carta2);
+printf("populacao: %lu\n", populacao_carta2);
+printf("area: %.2f km2\n", area_carta2);
+printf("pib: %.2f trilhoes\n", pib_carta2);
+printf("pontos turisticos: %d\n", pontos_turisticos_carta2);
+printf("densidade populacional:%.2f hab/km2\n", densidade_populacional_carta2);
+printf("pib per capita: %.2f trilhoes/hab\n", pib_per_capita_carta2);
+printf("super poder: %.2f\n", super_poder_carta2);
+//comparacao de atributos
+printf("\n--- comparacao de atributos (1= carta 1 vence, 0 = carta 2 vence) ---\n");
+printf("populacao: %d\n", populacao_carta1 > populacao_carta2 ? 1 : 0);
+printf("area: %d\n", area_carta1 > area_carta2 ? 1 : 0);
+printf("pib: %d\n", pib_carta1 > pib_carta2 ? 1: 0);
+printf("pontos turisticos: %d\n", pontos_turisticos_carta1 > pontos_turisticos_carta2 ? 1 : 0);
+printf(" densdade ppulacional: %d\n", densidade_populacional_carta1 > densidade_populacional_carta2 ? 1 : 0);
+printf("pib per capita %d\n", pib_per_capita_carta1 > pib_per_capita_carta2 ? 1 :0);
+printf("super poder: %d\n", super_poder_carta1 > super_poder_carta2 ? 1 : 0);
+return 0;
+
 
 
 
